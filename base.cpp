3cc0384bb8a0f4ae9096e119b48be03cfb0175c0
kunal <singalkunal10@gmail.com>
2020-10-27 02:28:00 +0530

    add markup file

---
+++ base.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define KUNAL
+#define ll          long long
+#define all(n)      for(int i=0; i<int(n); i++)
+#define rep(i,a,b)  for(int i=a; i<=(int)b; i++)
+#define rrep(i,a,b) for(int i=a; i>=(int)b; i--)
+#define pb          push_back
+#define pii         pair<int, int>
+#define pll         pair<ll, ll>
+#define vi          vector<int>
+#define vvi         vector<vi>
+#define vll         vector<ll>
+#define F           first
+#define S           second
+#define             endl '\n'
+#define FIO         ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+void test_case() {
+
+}
+
+int main(){
+    FIO;
+
+    int t; cin>>t;
+    while(t--) test_case();
+}
+
